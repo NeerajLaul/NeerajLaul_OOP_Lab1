f6fe9e69aa000394adbebcbb2a82e1c848a8d937
NeerajLaul <135740179+NeerajLaul@users.noreply.github.com>
2023-06-06 23:57:06 +0530

    Delete Lab_Assignment directory

--- Lab_Assignment/src/com/admin/service/EmpServ.java
+++
@@ -1,8 +0,0 @@
-package com.admin.service;
-
-import com.admin.employee.EmployeeDetails;
-public interface EmpServ {
-	String generateEmail(String firstname, String lastname, String dept);
-	String generatePassword();
-	void showDetails(EmployeeDetails e1);
-}
